8ca46f55369a52e59263ddf16a770b1f5dd9d560
PashenkoDaniil1605 <75112503+PashenkoDaniil1605@users.noreply.github.com>
2020-11-27 09:47:32 +0200

    Create DucksStopGoClass.cs

---
+++ DucksStopGoClass.cs
@@ -0,0 +1,11 @@
+using System;
+namespace ConsoleApp1
+{
+class DucksStopGo : IGo
+    {
+        public void Go()
+        {
+            Console.WriteLine("Я стою на месте!");
+        }
+    }
+}
