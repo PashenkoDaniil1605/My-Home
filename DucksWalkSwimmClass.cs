b132165bfcda77c326ce766eef58d824bf191085
PashenkoDaniil1605 <75112503+PashenkoDaniil1605@users.noreply.github.com>
2020-11-27 09:45:09 +0200

    Create DucksWalkSwimmClass.cs

---
+++ DucksWalkSwimmClass.cs
@@ -0,0 +1,11 @@
+using System;
+namespace ConsoleApp1
+{
+class DucksWalkSwimm : ISwimm
+    {
+        public void Swimm()
+        {
+            Console.WriteLine("Я плаваю на поверхности!");
+        }
+    }
+ }
