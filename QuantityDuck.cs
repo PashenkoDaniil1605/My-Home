738d23b0f0dac9e7270158de354d7f2de3d49b63
PashenkoDaniil1605 <75112503+PashenkoDaniil1605@users.noreply.github.com>
2020-11-27 10:03:27 +0200

    Create QuantityDuck.cs

---
+++ QuantityDuck.cs
@@ -0,0 +1,13 @@
+using System;
+namespace ConsoleApp1
+{
+class QuantityDuck : QuantityDuckBase
+    {
+
+        public QuantityDuck(List<ADuck> list)
+        {
+            _ducks = list;
+        }
+
+    }
+}
