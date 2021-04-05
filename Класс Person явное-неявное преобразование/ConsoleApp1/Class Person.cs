d1206359ccd23950f7ca259ec8c90eabf6dff295
PashenkoDaniil1605 <75112503+PashenkoDaniil1605@users.noreply.github.com>
2021-04-05 10:51:49 +0300

    Add files via upload

---
+++ Класс Person явное-неявное преобразование/ConsoleApp1/Class Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        // Явное приведение объекта Person к типу string
+        public static explicit operator string(Person ob)
+        {
+            return string.Format($"{ob.FirstName} {ob.LastName}");
+        }
+        // Неявное приведение типа string к объекту Person
+        public static implicit operator Person(string ob)
+        {
+            string[] arr = ob.Split(new char[] { ' ' });
+            return new Person(arr[0], arr[1]);
+        }
+    }
+}
---
+++ Класс Person явное-неявное преобразование/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Вася", "Пупкин");
+            string str = (string)person;
+            Console.WriteLine(str);
+
+            person = "Петр Долгопрудный";
+            Console.WriteLine($"{person.FirstName} {person.LastName}");
+
+            if ((string)person == "Петр Долгопрудный")
+                Console.WriteLine("== объекты равны");
+
+            if (Equals((string)person, "Петр Долгопрудный"))
+                Console.WriteLine("Equals() вернул true");
+
+            if (!Equals(person, "Петр Долгопрудный"))
+                Console.WriteLine("Equals() вернул false");
+
+            Console.ReadKey();
+        }
+    }
+}
